010f5a4e38864f0bb015577c22f3c2627cfea391
stevecyj <ycsteven.tsao@gmail.com>
2022-05-06 06:35:25 +0800

    static files

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 
 // set port
 const PORT = 3000;
+
+// 引入 express 讀取靜態資源
+app.use(express.static(__dirname + "/public"));
+
+// GET
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello my express server");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
